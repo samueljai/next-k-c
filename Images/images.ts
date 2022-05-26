f8d0fcec85ed70266067b95d3bf7a332c92d4ad3
Sam Desai <samueljaidesai@gmail.com>
2022-05-26 12:09:08 +0100

    move star image to public folder

--- Images/images.ts
+++
@@ -1,9 +0,0 @@
-import star from './star.png';
-
-const images: any = {
-    star,
-};
-
-export function getImage(imageName: string) {
-    return images[imageName];
-}
--- components/Common/Stars/Star.tsx
+++ components/Common/Stars/Star.tsx
@@ -1,11 +1,10 @@
-import { getImage } from '../../../Images/images';
 import Image from 'next/image';
 import styles from './star.module.scss';
 
 const Star = () => {
     return (
         <div className={styles.star__image}>
-            <Image src={getImage('star')} alt="star" width={15} height={15} />
+            <Image src="/star.png" alt="star" width={15} height={15} />
         </div>
     );
 };
